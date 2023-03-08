7671e21d2618731b3f11a7dec86ea8fd2ec216d8
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-03-08 16:06:39 +0630

    Actions

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,10 @@
-console.log("From index.js")
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+// action creator that returns action
+function orderCake() {
+  // action object
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
